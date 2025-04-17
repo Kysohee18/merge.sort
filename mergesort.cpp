ffe7c0a4a35fd77ddf36907648f73a3f095fe809
Kyosohee18 <hanashahky@gmail.com>
2025-04-17 10:43:34 +0700

    void input element

---
+++ mergesort.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int arr[20], B[20]
+
+void input() {
+    while(true){
+        cout << "masukkan panjang  element array:";
+        cin >> n;
+
+        if(n<=20){
+            break;
+        }
+        else {
+            cout <<\"nMaksimal panjang array adalah 20";
+        }
+    }
+
+
+
+    cout << "\n====================";
+    cout << "\nInputkan Isi Element";
+    cout << "\=====================" <<endl;
+
+    for (int i = o; i < n; i++) {
+        cout <<"array index ke -" << i << ":";
+        cin >> arr[i];
+    }
+
+
+
+
+
+
+
+}
+
